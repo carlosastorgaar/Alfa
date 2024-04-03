cee7493226dd911a56019e76ba7bcc1daf026b67
Carlos Astorga <31820361+carlosastorgaar@users.noreply.github.com>
2024-04-03 10:34:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function showInfo() {
         var firstName = document.getElementById("firstName").value;
         var lastName = document.getElementById("lastName").value;
         var fullName = firstName + " " + lastName;
-        document.getElementById("result").innerText = "Nombre completo: " + fullName;
+        
+        
+        
+        document.getElementById("result").innerText =` El nombre completo es: ${fullName} `
     }
 
 
